5438f6f5ccda319d1feb69e4bf29ba5fe03c0690
CAMB <50176265+camilomoreba83@users.noreply.github.com>
2021-11-10 08:22:25 -0500

    se modifica firebase

--- app/src/main/java/co/edu/unab/mercapp/FireBaseActivity.java
+++ app/src/main/java/co/edu/unab/mercapp/FireBaseActivity.java
@@ -3,6 +3,7 @@ package co.edu.unab.mercapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FireBaseActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class FireBaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_base);
+    }
+    public void volver(View view){
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
